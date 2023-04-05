756c12e411209c9f76a0d0b754b3fa453e8bc8ae
GobbleyDuck <ethan.molsberry@gmail.com>
2023-04-05 19:20:33 -0400

    Gate files without erros

--- Final_Prject_Object_Oriented/gate.cpp
+++ Final_Prject_Object_Oriented/gate.cpp
@@ -1,1 +1,14 @@
 #include "gate.h"
+
+void Gate::setType(string type) {
+	this->type = type;
+}
+
+void Gate::setDelay(int delay) {
+	this->delay = delay;
+}
+
+//TODO prepare as a vector
+void Gate::setInput(int inputNumber, Wire* input) {
+
+}
--- Final_Prject_Object_Oriented/gate.h
+++ Final_Prject_Object_Oriented/gate.h
@@ -1,8 +1,33 @@
 #pragma once
-//#include "wire.h"
+#include "wire.h"
+#include <string>
+#include <vector>
+
+using namespace std;
 
 class Wire;
 
 class Gate {
+public:
+	Gate(string type, int delay, Wire* output, Wire* input1, Wire* input2) {};
 
+	//getters
+	int getDelay()const {return delay;}
+
+	Wire* getInput(int input)const {return inputs[input];}
+
+	Wire* getOutput()const {return output;}
+
+	//function prototypes
+	void setType(string type);
+	void setDelay(int delay);
+	void setInput(int inputNumber, Wire* input);
+
+	//TODO: returnVal evaluate(parms)
+
+private:
+	string type;
+	int delay;
+	Wire* output;
+	vector<Wire*> inputs;
 };
