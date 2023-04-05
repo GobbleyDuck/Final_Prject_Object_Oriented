41e7fb272f58343879c12e0ab4cbf51fab220390
22may <22may@DESKTOP-V86C891>
2023-04-05 16:04:47 -0400

    uploaded wire.h

--- Final_Prject_Object_Oriented/wire.h
+++ Final_Prject_Object_Oriented/wire.h
@@ -1,1 +1,38 @@
+//Natalie Mayfield and Ethan Molsberry
+
 #pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+class Gate;
+
+class Wires {
+
+public:
+	Wires(string n = "", char v = '\0', Gate* gate = nullptr, int i = 0, string h = "");	//constructor
+
+	string getName()const { return name; }
+	char getValue()const { return value; }
+	vector<Gate*> getDrives() const { return drives; }
+	int getIndex() const { return index; }
+	string getHistory() const { return history; }
+
+	void addGate(vector <Gate*>);
+	void setState(string);
+	void setHistory(string, int);
+
+	void printHistory();
+
+private:
+	string name;
+	string state;
+	vector <Gate*> drives;
+	int index;
+	string history;
+	int time;
+
+};
