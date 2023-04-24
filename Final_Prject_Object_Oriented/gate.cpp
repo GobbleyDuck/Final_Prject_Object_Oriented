c27ebb1eb8b254a2f386f5d4d667c352e696914d
22may <22may@DESKTOP-V86C891>
2023-04-24 09:38:33 -0400

    changes to evaluate() in gate.cpp

--- Final_Prject_Object_Oriented/gate.cpp
+++ Final_Prject_Object_Oriented/gate.cpp
@@ -29,90 +29,79 @@ bool Gate::evaluate()const {
 	if (type == "NOT") {
 		if (inputs[0]->getState() == '1') {
 			output->setState('0');
+			return false;
 		}
 		else if (inputs[0]->getState() == '0') {
 			output->setState('1');
-		}
-		else {
-			output->setState('X');
-		}
-
-		if (output->getState() == '1') {
 			return true;
 		}
 		else {
+			output->setState('X');
 			return false;
 		}
+
 	}
 	//or gate
 	if (type == "OR") {
-		bool output_state = false;
 		for (int i = 0; i < inputs.size(); i++) {
 			if (inputs[i]->getState() == '1') {
-				output_state = true;
-				break;
+				output->setState('1');
+				return true;
 			}
 			else if (inputs[i]->getState() == 'X') {
-				output_state = true;
+				output->setState('1');
+				return true;
+			}
+			else {
+				output->setState('0');
+				return false;
 			}
 		}
-		if (output_state) {
-			output->setState('1');
-		}
-		else {
-			output->setState('0');
-		}
-		return output_state;
+		
 	}
 
 	//xor gate
 	if (type == "XOR") {
-		bool output_state = false;
 
 		//true if outputs diff and false if same
 		if (inputs[0]->getState() != inputs[1]->getState()) {
-			output_state = true;
 			output->setState('1');
+			return true;
 		}
 		else {
 			output->setState('0');
+			return false;
 		}
 
-		return output_state;
 
 	}
 
 	//nand gate
 	if (type == "NAND") {
-		bool output_state = false;
 		// false if both inputs are a 1, and true otherwise
 		if (inputs[0]->getState() == 0 || inputs[1]->getState() == 0) {
 			output->setState('0');
+			return false;
 		}
 		else {
 			output->setState('1');
-			output_state = true;
+			return true;
 		}
-
-		return output_state;
 	}
-
 
 	//xnor gate
 
 	if (type == "XNOR") {
-		bool output_state = false;
 
 		//true if both outputs are the same and false if diff
 		if (inputs[0]->getState() == inputs[1]->getState()) {
-			output_state = true;
 			output->setState('1');
+			return true;
 		}
 		else {
 			output->setState('0');
+			return false;
 		}
-		
-		return output_state;
 
 	}
 
