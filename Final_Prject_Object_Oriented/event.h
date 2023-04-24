ff16c8318cb754caad5992c714425b507047f7b3
GobbleyDuck <ethan.molsberry@gmail.com>
2023-04-24 16:13:28 -0400

    uh why?

--- Final_Prject_Object_Oriented/event.h
+++ Final_Prject_Object_Oriented/event.h
@@ -1,12 +1,20 @@
-#pragma once
+
+
 #include <iostream>
+
+#ifndef EVENT_H
+#define EVENT_H
+
 #include "wire.h"
+#include "gate.h"
 
 using namespace std;
 
 
 class Event {
 public:
+	//added new Even constructor
+	Event(const vector<Wire>& inputs, const int timings[3]) : inputs(inputs), timings{ timings[0], timings[1], timings[2] } {}
 
 	Event(Wire* w, int c, int t, char v) { 
 		wire = w; 
@@ -15,16 +23,19 @@ public:
 		value = v;
 	}
 
-	bool operator<(Event rhs) {
+	/*bool operator<(Event rhs) {
 		return time > rhs.time;
-	}
+	}*/
 
 
 
 
-
+	vector<Wire> inputs;
+	int timings[3];
 	int time;
 	int count;
 	char value;
 	Wire* wire;
 };
+
+#endif 
--- Final_Prject_Object_Oriented/gate.cpp
+++ Final_Prject_Object_Oriented/gate.cpp
@@ -15,94 +15,95 @@ void Gate::setInput(Wire* input) {
 }
 
 //TODO: make Wire class complete (causing issues)
-bool Gate::evaluate()const {
-	//and gate
-	if (type == "AND") {
-		for (int i = 0; i < inputs.size(); i++) {
-			if (!inputs[i]->getState()) {
-				return false;
-			}
-		}
-		return true;
-	}
-	//not gate
-	if (type == "NOT") {
-		if (inputs[0]->getState() == '1') {
-			output->setState('0');
-			return false;
-		}
-		else if (inputs[0]->getState() == '0') {
-			output->setState('1');
-			return true;
-		}
-		else {
-			output->setState('X');
-			return false;
-		}
+bool Gate::evaluate() const {
+    // and gate
+    if (type == "AND") {
+        for (int i = 0; i < static_cast<int>(inputs.size()); i++) {
+            if (!inputs[i]->getState()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	}
-	//or gate
-	if (type == "OR") {
-		for (int i = 0; i < inputs.size(); i++) {
-			if (inputs[i]->getState() == '1') {
-				output->setState('1');
-				return true;
-			}
-			else if (inputs[i]->getState() == 'X') {
-				output->setState('1');
-				return true;
-			}
-			else {
-				output->setState('0');
-				return false;
-			}
-		}
-		
-	}
+    // not gate
+    if (type == "NOT") {
+        if (inputs[0]->getState() == '1') {
+            output->setState('0');
+            return false;
+        }
+        else if (inputs[0]->getState() == '0') {
+            output->setState('1');
+            return true;
+        }
+        else {
+            output->setState('X');
+            return false;
+        }
+    }
 
-	//xor gate
-	if (type == "XOR") {
+    // or gate
+    if (type == "OR") {
+        bool output_state = false;
+        for (int i = 0; i < inputs.size(); i++) {
+            if (inputs[i]->getState() == '1') {
+                output_state = true;
+                break;
+            }
+            else if (inputs[i]->getState() == 'X') {
+                output_state = true;
+            }
+        }
+        if (output_state) {
+            output->setState('1');
+        }
+        else {
+            output->setState('0');
+        }
+        return output_state;
+    }
 
-		//true if outputs diff and false if same
-		if (inputs[0]->getState() != inputs[1]->getState()) {
-			output->setState('1');
-			return true;
-		}
-		else {
-			output->setState('0');
-			return false;
-		}
+    // xor gate
+    if (type == "XOR") {
+        bool output_state = false;
+        // true if outputs diff and false if same
+        if (inputs[0]->getState() != inputs[1]->getState()) {
+            output_state = true;
+            output->setState('1');
+        }
+        else {
+            output->setState('0');
+        }
+        return output_state;
+    }
 
+    // nand gate
+    if (type == "NAND") {
+        bool output_state = false;
+        // true if both inputs are a 1, and false otherwise
+        if (inputs[0]->getState() == '1' && inputs[1]->getState() == '1') {
+            output->setState('0');
+            output_state = true;
+        }
+        else {
+            output->setState('1');
+        }
+        return output_state;
+    }
 
-	}
+    // xnor gate
+    if (type == "XNOR") {
+        bool output_state = false;
+        // true if both outputs are the same and false if diff
+        if (inputs[0]->getState() == inputs[1]->getState()) {
+            output_state = true;
+            output->setState('1');
+        }
+        else {
+            output->setState('0');
+        }
+        return output_state;
+    }
 
-	//nand gate
-	if (type == "NAND") {
-		// false if both inputs are a 1, and true otherwise
-		if (inputs[0]->getState() == 0 || inputs[1]->getState() == 0) {
-			output->setState('0');
-			return false;
-		}
-		else {
-			output->setState('1');
-			return true;
-		}
-	}
-
-	//xnor gate
-
-	if (type == "XNOR") {
-
-		//true if both outputs are the same and false if diff
-		if (inputs[0]->getState() == inputs[1]->getState()) {
-			output->setState('1');
-			return true;
-		}
-		else {
-			output->setState('0');
-			return false;
-		}
-
-	}
-
+    return false;
 }
