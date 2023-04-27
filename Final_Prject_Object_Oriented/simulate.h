90dcf66157b665f877881a7c3f78d4c38fdbe367
kshomper <kshomper@cedarville.edu>
2023-04-27 08:49:12 -0400

    Added comments in simulation loop to get team back on track

--- Final_Prject_Object_Oriented/simulate.h
+++ Final_Prject_Object_Oriented/simulate.h
@@ -25,13 +25,29 @@ public:
 			eventQueue.pop();
 			time = currEvent.getTime();
 			
-			//check if event creates a change in the wire - if no, discard event
-			if (currEvent.getValue() == currWire->getState()) {
-				continue;
-			}
+			// the code commented out below is altogether incorrect, use the comments
+			// I've written that follow to get back on track - KAS
+			// check if event creates a change in the wire - if no, discard event
+			// if (currEvent.getValue() == currWire->getState()) {
+			// 	continue;
+			// }
 
+			// currWire may drive a Gate (or Gates). If it does, the simulation
+			// needs to determine if the output of the Gate will change based on
+			// the change to currWire. Therefore, we need to do the following:
+			// 1. Loop over all the Gates that currWire drives
+			// 2. Within this loop determine what the output of the Gate is
+			//    presently, that is, without the change to currWire. Name that
+			//    output value something like presentOutput.
+			// 3. Also within the loop determine what the output of the Gate
+			//    would be if we used the new value for the currWire, rather than
+			//    the present value. Name that output value futureOutput
+			// 4. Then compare presentOutput with futureOutput. If they are
+			//    different, then we need to insert the futureOutput event in the
+			//    simulation queue. The event is:
+			//      Event(outputWire, count, time+Gate.delay, futureOutput)
 
-
+			// the event wire should never be nullptr, so the if is unnecessary - KAS
 			if (currEvent.GetEventWire() != nullptr) {
 				currWire->setHistory(currWire->getState(), currEvent.getTime() - 1);
 				currWire->setState(currEvent.getValue());
